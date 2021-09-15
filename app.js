a1affc1343579200e683671667be8582216bcee3
sloth-grrr <35689080+sloth-grrr@users.noreply.github.com>
2021-09-15 11:03:52 +0530

    taking input

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var userName = prompt("What is your name?")
-alert("Alert hello " + userName);
+// var userName = prompt("What is your name?")
+// alert("Alert hello " + userName);
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+function clickHandler() {
+    console.log("Clicked!");
+    console.log("input", txtInput.value);
+};
+
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
