65e9efa75306f5078b05c409d89005dec6479881
sloth-grrr <35689080+sloth-grrr@users.noreply.github.com>
2021-09-15 12:13:41 +0530

    input output processing done

--- app.js
+++ app.js
@@ -3,13 +3,21 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
     console.log("Clicked!");
     console.log("input", txtInput.value);
+    outputDiv.innerText = "bababababbananananna " + txtInput.value;
 };
 
 
 btnTranslate.addEventListener("click", clickHandler)
 
-
+/**
+ * Exercise:
+ * 1. document.querySelector("textarea");
+ * 2. .btn-primary
+ * 3. #input-btn
+ * 4. input[name='translator'];
+ */
