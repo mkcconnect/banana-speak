2fc200972f3b2b3caf2ab4df9cde8b421632cc54
sloth-grrr <35689080+sloth-grrr@users.noreply.github.com>
2021-09-20 23:30:53 +0530

    fixed css link file

--- app.js
+++ app.js
@@ -5,19 +5,40 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL (text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler (error){
+    console.log("Error occurred: ", error);
+    alert("Something is wrong. Try again later.")
+}
+
+
 function clickHandler() {
-    console.log("Clicked!");
-    console.log("input", txtInput.value);
-    outputDiv.innerText = "bababababbananananna " + txtInput.value;
+    var inputText = txtInput.value; //taking input
+
+    // console.log("Clicked!");
+    // console.log("input", txtInput.value);
+    // outputDiv.innerText = "bababababbananananna " + txtInput.value;
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
 };
 
 
 btnTranslate.addEventListener("click", clickHandler)
 
-/**
- * Exercise:
- * 1. document.querySelector("textarea");
- * 2. .btn-primary
- * 3. #input-btn
- * 4. input[name='translator'];
- */
+// /**
+//  * Exercise:
+//  * 1. document.querySelector("textarea");
+//  * 2. .btn-primary
+//  * 3. #input-btn
+//  * 4. input[name='translator'];
+//  */
